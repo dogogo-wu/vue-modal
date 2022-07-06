bd71c54f61de05271671b659d1c886ad40e612e4
dogogo-wu <dog9zero0@gmail.com>
2022-07-06 14:11:55 +0800

    video-5
    props, emit, event modifier, slot, teleport

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,71 @@
 <template>
   <h1>{{ title }}</h1>
-  <br>
-  <Modal v-if="isClick" />
-  <input type="text" ref="name">
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <!-- Using Props -->
+    <!-- <Modal :mtitle="header" :text="text" theme="sale" @close="toggleModal" /> -->
+
+    <!-- Using Slot -->
+    <Modal theme="" @close="toggleModal">
+      <h1>Modal Title</h1>
+      <p>My content</p>
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">More info</a>
+      </template>
+    </Modal>
+  </div>
+
+  <teleport to=".modals" v-if="showModal2">
+    <Modal theme="sale" @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">More info</a>
+      </template>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModal2">Open Modal2</button>
+  <br /><br />
+  <input type="text" ref="name" />
   <button @click="handleClick">Click me</button>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
-  components:{
-    Modal
+  components: {
+    Modal,
   },
   data() {
     return {
-      title: 'My First Vue App!',
-      isClick: false
+      title: "My First Vue App!",
+      showModal: false,
+      showModal2: false,
+      header: "Modal Title",
+      text: "My content",
     };
   },
   methods: {
     handleClick() {
       console.log(this.$refs.name);
-      this.$refs.name.classList.add('active');
+      this.$refs.name.classList.add("active");
       this.$refs.name.focus();
-      this.isClick = !this.isClick;
-    }
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
   },
 };
 </script>
 
 <style>
-#app {
+#app, .modals{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -40,7 +73,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1{
+h1 {
   display: inline-block;
   border-bottom: 3px solid #45a2ff;
   padding-bottom: 5px;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,24 +1,71 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
-            <p>My content</p>
-        </div>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{sale: theme==='sale'}">
+      <!-- <h1>{{ mtitle }}</h1>
+      <p>{{ text }}</p> -->
+
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
+  </div>
 </template>
 
-<style scoped>
-    .backdrop{
-        top:0;
+<script>
+    export default {
+        props:[ 'mtitle', 'text', "theme" ],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        },
+    }
+</script>
+
+<style>
+    .backdrop {
+        top: 0;
         position: fixed;
         background: rgba(0, 0, 0, 0.5);
         width: 100%;
         height: 100%;
     }
-    .modal{
+    .modal {
         width: 400px;
         padding: 20px;
         margin: 100px auto;
         background: white;
         border-radius: 10px;
+    }
+    .modal h1 {
+        color: rgb(36, 185, 135);
+        border: none;
+        padding: 0;
+    }
+    .modal.sale{
+        background-color: rgb(240, 126, 126);
+        color: white;
+    }
+    .modal.sale h1{
+        color: white;
+    }
+    .modal .actions{
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+    .modal .actions a{
+        color: rgb(64, 64, 64);
+        padding: 8px;
+        border: 1px solid rgb(212, 212, 212);
+        border-radius: 5px;
+        text-decoration: none;
+        margin: 10px;
+    }
+    .modal.sale .actions{
+        color: white;
+    }
+    .modal.sale .actions a{
+        color: white;
     }
 </style>
